03fd9d681a009efd9895b384ed6d96df2e60e501
Tina Huang <thuang@berkeley.edu>
2016-01-20 12:01:49 -0800

    procfile changes

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn twitlet.wsgi
+web: gunicorn mysite.wsgi
--- wgsi.py
+++ wgsi.py
@@ -1,4 +1,7 @@
 import os
-os.environ.setdefault(“DJANGO_SETTINGS_MODULE”, “APP.settings”)
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "APP.settings")
+
+# This application object is used by the development server
+# as well as any WSGI server configured to use this file.
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
